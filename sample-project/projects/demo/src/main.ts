b3af6f1dc3aac640e05d8dfbb92f9d32dd27af0b
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-09-27 02:52:32 +0900

    use same scoped zone in demo and elements

--- sample-project/projects/demo/src/main.ts
+++ sample-project/projects/demo/src/main.ts
@@ -1,14 +1,38 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
+const compilerOptions: any = environment.zoneLess ? { ngZone: 'noop' } : {};
+if (environment.zoneLess) {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule, compilerOptions)
+    .catch(err => console.error(err));
+} else {
+  const Zone = (window as any)['Zone'];
+  try {
+    // init zone
+    Zone.__init__();
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .then(ref => {
+        // Ensure Angular destroys itself on hot reloads.
+        if (window['ngRef']) {
+          window['ngRef'].destroy();
+        }
+        window['ngRef'] = ref;
 
-const compilerOptions: any = environment.zoneLess ? {ngZone: 'noop'} : {};
-platformBrowserDynamic().bootstrapModule(AppModule, compilerOptions)
-  .catch(err => console.error(err));
+        // Otherise, log the boot error
+      })
+      .catch(err => console.error(err));
+  } finally {
+    // unload all monkey-patch, so Window/Global API will be
+    // reverted to native one. No impact to outside world.
+    Zone.__unloadAll_patch();
+  }
+}
